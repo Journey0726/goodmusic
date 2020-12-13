237e1df71b3cf037295bf6aa320bea5822659cb3
chenhui <1549120948@qq.com>
2020-12-13 22:42:08 +0800

    简易获取歌曲信息

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <player></player>
+    <button @click="getSearchInfo('许嵩')">dianwo </button>
+  
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import {getSearchInfo} from '@/network/search.js'
+import player from '@/views/player.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    player
+  },
+ 
+    Playerethods:{
+    getSearchInfo(info){
+      getSearchInfo(info).then(res=>{
+        // this.list = res.result.songs
+        console.log(res);
+      })
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./assets/css/base.css";
+
 </style>
---
+++ src/views/player.vue
@@ -0,0 +1,14 @@
+<template>
+    <audio src="https://music.163.com/song/media/outer/url?id=33894312.mp3"  controls="controls"></audio>
+
+</template>
+
+<script>
+export default {
+name:'player',
+}
+</script>
+
+<style>
+
+</style>
